c2972b1fac15105abf57b2b5b22b57ee45c140a2
diogo.r.silva <diogo.r.silva@accenture.com>
2018-07-18 10:32:34 -0300

    feature(repositories): Add repository pattern

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,10 +3,11 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 const ValidatorContract = require('../validators/fluent-validator');
+const repository = require('../repositories/products-repository');
 
 exports.get = (req, res, next) => {
-    Product
-        .find({ active: true }, 'title price slug')
+    repository
+        .get()
         .then(data => {
             res.status(200).send(data);
         })
@@ -16,8 +17,8 @@ exports.get = (req, res, next) => {
 }
 
 exports.getById = (req, res, next) => {
-    Product
-        .findById(req.params.id)
+    repository
+        .getById(req.params.id)
         .then(data => {
             res.status(200).send(data);
         })
@@ -27,11 +28,8 @@ exports.getById = (req, res, next) => {
 }
 
 exports.getBySlug = (req, res, next) => {
-    Product
-        .findOne({
-            slug: req.params.slug,
-            active: true
-        }, 'title description price slug tags')
+    repository
+        .getBySlug(req.params.slug)
         .then(data => {
             res.status(200).send(data);
         })
@@ -41,11 +39,8 @@ exports.getBySlug = (req, res, next) => {
 }
 
 exports.getByTag = (req, res, next) => {
-    Product
-        .find({
-            tags: req.params.tag,
-            active: true
-        }, 'title description price slug tags')
+    repository
+        .getByTag(req.params.tag)
         .then(data => {
             res.status(200).send(data);
         })
@@ -60,14 +55,13 @@ exports.post = (req, res, next) => {
     contract.hasMinLen(req.body.slug, 3, 'The slug must contain at least 3 character');
     contract.hasMinLen(req.body.description, 3, 'The description must contain at least 3 character');
 
-    if(!contract.isValid()){
+    if (!contract.isValid()) {
         res.status(400).send(contract.errors()).end();
         return;
     }
- 
-    var product = new Product(req.body);
-    product
-        .save()
+
+    repository
+        .create(req.body)
         .then(x => {
             res.status(201).send({
                 message: 'Product successfully registered!'
@@ -82,14 +76,8 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    Product
-        .findByIdAndUpdate(req.params.id, {
-            $set: {
-                title: req.body.title,
-                description: req.body.description,
-                price: req.body.price
-            }
-        })
+    repository
+        .update(req.params.id, req.body)
         .then(x => {
             res.status(200).send({
                 message: 'Product successfully updated!'
