ef1ac433f78090e61395eb5048e3fdf75f7dd465
diogo.r.silva <diogo.r.silva@accenture.com>
2018-07-20 09:59:10 -0300

    feature(auth): Add recover token to ask a order

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -2,6 +2,7 @@
 
 const repository = require('../repositories/order-repository');
 const uuidv1 = require('uuid/v1');
+const authService = require('../services/auth-service');
 
 exports.get = async (req, res, nest) => {
     try{
@@ -9,15 +10,21 @@ exports.get = async (req, res, nest) => {
         res.status(200).send(data);
     } catch (e) {
         res.status(500).send({
-            message: 'Falha ao processar sua requisição'
+            message: 'Failed to process your request!'
         })
     }
 }
 
 exports.post = async (req, res, next) => {
     try {
+        // Recover Token
+        var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+        // Decode Token
+        var data = await authService.decodeToken(token);
+
         await repository.create({
-            customer: req.body.customer,
+            customer: data.id,
             number: uuidv1(),
             items: req.body.items
         });
